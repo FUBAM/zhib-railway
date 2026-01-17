4998dcfda98e8701a9c5dc164bf198867abd89e9
Habib Farhan <habibfarhan6788@gmail.com>
2026-01-17 14:53:21 +0700

    seederss

---
+++ database/seeders/ResetDatabaseSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class ResetDatabaseSeeder extends Seeder
+{
+    public function run(): void
+    {
+        Schema::disableForeignKeyConstraints();
+
+        $tables = [
+            'users',
+            'password_reset_tokens',
+            'sessions',
+            'kategori',
+            'kota',
+            'komunitas',
+            'anggota_komunitas',
+            'events',
+            'peserta_kegiatan',
+            'grup',
+            'pesan_grup',
+            'laporan',
+            'badge',
+            'badge_user',
+            'pembayaran',
+            'pemeritahuanan',
+            'berita',
+        ];
+
+        foreach ($tables as $table) {
+            DB::table($table)->truncate();
+        }
+
+        Schema::enableForeignKeyConstraints();
+    }
+}
